49277dbffe432f2f25fa0efa44dc0134db704e0b
pacman29 <balyakin.d.v@gmail.com>
2017-02-23 18:59:54 +0300

    first commit

---
+++ public/static/views/parts/Border.js
@@ -0,0 +1,48 @@
+/**
+ * Created by pacman29 on 22.02.17.
+ */
+(function () {
+    window.Border = function Border(img){
+        let container = document.createElement('div');
+        container.setAttribute('class','container border');
+        container.innerHTML = `
+            <div class="row">
+                <div class="col-lg-2"></div>
+                <div class="col-lg-6">
+                    <div id="inside_block" >
+                        
+                    </div>
+                </div>
+                <div class="col-lg-1"></div>
+                <div class="col-lg-3">
+                    <div class="row">
+                        <img src=${img} alt="Placeholder image" class="img-responsive">
+                    </div>
+                </div>
+             </div>
+            `;
+        return container;
+    }
+});
+/*
+ <div class="container my_btn_image">
+    <div class="row">
+        <div class="col-lg-2">
+
+        </div>
+        <div class="col-lg-6">
+            <div id="singin_form" >
+                <div class="js-login" hidden="hidden"></div>
+                <div class="js-chat" hidden="hidden"></div>
+                <div class="js-reg" hidden="hidden"></div>
+                <script src="form2.js"></script>
+                <script src="form1.js"></script>
+            </div>
+        </div>
+        <div class="col-lg-1"></div>
+        <div class="col-lg-3">
+            <div class="row"><img src="../images/add_your_data.png" alt="Placeholder image" class="img-responsive my_img_sing_in"></div>
+        </div>
+    </div>
+ </div>
+ */
