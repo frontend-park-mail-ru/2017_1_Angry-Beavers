9038b3cd82a1db6132e0d102f220e77ed0481fe4
Ed Asriyan <asriyan1998@yandex.ru>
2017-03-10 14:49:58 +0300

    Add about us template

--- public/static/views/parts/Footer/Footer.js
+++ public/static/views/parts/Footer/Footer.js
@@ -2,11 +2,11 @@
  * Created by pacman29 on 22.02.17.
  */
 (function () {
-    let footer_template = template;
+    let footerTemplate = template;
     window.Footer = function (opt) {
         let footer = document.createElement('footer');
         footer.setAttribute('class', 'text-center footer');
-        footer.innerHTML = footer_template({_css: opt.class, text: opt.text});
+        footer.innerHTML = footerTemplate({_css: opt.class, text: opt.text});
         return footer;
     };
 }());
--- public/static/views/parts/Rules/Rules.js
+++ public/static/views/parts/Rules/Rules.js
@@ -2,11 +2,11 @@
  * Created by pacman29 on 08.03.17.
  */
 (function () {
-    let rules_template = template;
+    let rulesTemplate = template;
     window.Rules = function (opt) {
         debugger;
         let div = document.createElement('div');
-        div.innerHTML = rules_template({
+        div.innerHTML = rulesTemplate({
             text: opt.text
         });
         return div;
