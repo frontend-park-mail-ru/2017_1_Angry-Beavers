dc1543e66b62684c108def3880f996de0f270d66
Ed Asriyan <ed-asriyan@protonmail.com>
2017-03-10 14:49:58 +0300

    Add about us template

--- public/static/views/parts/AboutUs/AboutUs.js
+++ public/static/views/parts/AboutUs/AboutUs.js
@@ -2,22 +2,13 @@
  * Created by pacman29 on 08.03.17.
  */
 (function () {
+    let aboutUsTemplate = template;
     window.AboutUs = function (opt) {
         let div = document.createElement('div');
-        div.setAttribute('class', 'aboutus');
 
-        let h3 = document.createElement('h3');
-        h3.textContent = opt.text;
-        div.appendChild(h3);
-        opt.members.forEach(iter => {
-            let a = document.createElement('a');
-            a.textContent = iter.text;
-            for (let i in iter) {
-                if (i.toString() != iter.text.toString()) {
-                    a.setAttribute(i, iter[i]);
-                }
-            }
-            div.appendChild(a);
+        div.innerHTML = aboutUsTemplate({
+            members: opt.members,
+            text: opt.text
         });
 
         return div;
