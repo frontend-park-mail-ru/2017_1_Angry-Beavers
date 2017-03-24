b4c414ea644b828d871a1b642f04f1ebedd4889a
Ed Asriyan <ed-asriyan@protonmail.com>
2017-03-25 02:51:15 +0300

    Add Border template

---
+++ public/static/views/parts/Border/Border.js
@@ -0,0 +1,17 @@
+/**
+ * Created by pacman29 on 22.02.17.
+ */
+
+'use strict';
+
+(function () {
+    window.Border = function Border(opt = {}) {
+        let container = document.createElement('div');
+        container.innerHTML = pugBorder({
+            stylesheet: opt.stylesheet,
+            img: opt.img,
+            element: opt.el
+        });
+        return container;
+    }
+}());
