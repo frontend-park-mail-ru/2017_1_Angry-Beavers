5e8b1c1a2723eb9e5baee46a1ca5d7ac59b8d470
Ed Asriyan <ed-asriyan@protonmail.com>
2017-04-05 00:04:20 +0300

    КАМА

--- public/static/api/api.js
+++ public/static/api/api.js
@@ -4,43 +4,59 @@
 
 'use strict';
 
-const fetch = require("node-fetch");
+let isBrowser = typeof require === 'undefined';
 
-const _HOST = 'jokinghazardserver.herokuapp.com';
+if (!isBrowser) {
+    var fetch = require('node-fetch');
+}
 
-const _post = function (method, obj) {
-    const url = 'https://' + _HOST + "/api/" + method;
-    const initPomise = {
-        method: 'POST',
-        mode: 'cors',
-        credentials: 'include',
-        headers: {
-            'Content-type': 'application/json',
-        },
-        body: JSON.stringify(obj),
+(function () {
+    const _HOST = 'jokinghazardserver.herokuapp.com';
+
+    const _post = function (method, obj) {
+        const url = 'https://' + _HOST + "/api/" + method;
+        const initPomise = {
+            method: 'POST',
+            mode: 'cors',
+            credentials: 'include',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(obj),
+        };
+        return fetch(url, initPomise).then(response => {
+            return response.json();
+        }).then(response => {
+            if (!response.result) {
+                throw new Error(response.errorMsg);
+            } else {
+                return response.result;
+            }
+        });
     };
-    return fetch(url, initPomise).then(response => {
-        return response.json();
-    }).then(response => {
-        if (!response.result) {
-            throw new Error(response.errorMsg);
-        } else {
-            return response.result;
-        }
-    });
-};
 
-exports.login = function (login, password) {
-    return _post('user/login', {
-        pass: password,
-        userLogin: login
-    });
-};
+    const login = function (login, password) {
+        return _post('user/login', {
+            pass: password,
+            userLogin: login
+        });
+    };
 
-exports.signUp = function (login, email, password) {
-    return _post('user/signup', {
-        userLogin: login,
-        pass: password,
-        userMail: email
-    });
-};
+    const signUp = function (login, email, password) {
+        return _post('user/signup', {
+            userLogin: login,
+            pass: password,
+            userMail: email
+        });
+    };
+
+    if (isBrowser) {
+        window.Api = {
+            login: login,
+            signUp: signUp
+        };
+    } else {
+        exports.login = login;
+        exports.signUp = signUp;
+    }
+})();
