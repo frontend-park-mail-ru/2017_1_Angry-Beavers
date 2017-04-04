e3edb7811ae0dd6f2ff64337f2a24d9a69533dcb
Ed Asriyan <asriyan1998@yandex.ru>
2017-04-04 23:27:48 +0300

    КАМАКИН

--- public/static/api/api.js
+++ public/static/api/api.js
@@ -1,49 +1,46 @@
 /**
  * Created by ed on 08.03.17.
  */
+
 'use strict';
-(function () {
-    const _HOST = 'jokinghazardserver.herokuapp.com';
-    // const _HOST = 'localhost:5000';
-    const _post = function (method, obj) {
-        const url = 'https://' + _HOST + "/api/" + method;
-        const initPomise = {
-            method: 'POST',
-            mode: 'cors',
-            credentials: 'include',
-            headers: {
-                'Content-type': 'application/json',
-            },
-            body: JSON.stringify(obj),
-        };
-        return fetch(url, initPomise).then(response => {
-            return response.json();
-        }).then(response => {
-            if (!response.result) {
-                throw new Error(response.errorMsg);
-            } else {
-                return response.result;
-            }
-        });
-    };
 
-    const login = function (login, password) {
-        return _post('user/login', {
-            pass: password,
-            userLogin: login
-        });
-    };
+const fetch = require("node-fetch");
 
-    const signUp = function (login, email, password) {
-        return _post('user/signup', {
-            userLogin: login,
-            pass: password,
-            userMail: email
-        });
-    };
+const _HOST = 'jokinghazardserver.herokuapp.com';
 
-    window.Api = {
-        login: login,
-        signUp: signUp
+const _post = function (method, obj) {
+    const url = 'https://' + _HOST + "/api/" + method;
+    const initPomise = {
+        method: 'POST',
+        mode: 'cors',
+        credentials: 'include',
+        headers: {
+            'Content-type': 'application/json',
+        },
+        body: JSON.stringify(obj),
     };
-}());
+    return fetch(url, initPomise).then(response => {
+        return response.json();
+    }).then(response => {
+        if (!response.result) {
+            throw new Error(response.errorMsg);
+        } else {
+            return response.result;
+        }
+    });
+};
+
+exports.login = function (login, password) {
+    return _post('user/login', {
+        pass: password,
+        userLogin: login
+    });
+};
+
+exports.signUp = function (login, email, password) {
+    return _post('user/signup', {
+        userLogin: login,
+        pass: password,
+        userMail: email
+    });
+};
