50768657335f0c74dd9ee6ac84d7531d650ef29e
Ed Asriyan <ed-asriyan@protonmail.com>
2017-04-06 01:17:59 +0300

    Add session into the views

--- public/static/controllers/MenuStartController.js
+++ public/static/controllers/MenuStartController.js
@@ -7,9 +7,7 @@
 (function () {
     class MenuStartController extends window.View {
         constructor(opt = {}) {
-            super(opt.user);
-            this.user = opt.user;
-            this.page_parts = opt.page.getParts();
+            super(opt);
             this.addListener();
         }
 
@@ -39,7 +37,7 @@
 
         show() {
             this.page_parts.get("AppName").hidden = false;
-            if (this.user.isAuth) {
+            if (this.session.isAuth) {
                 this.router.go("/menu");
             }
             else {
