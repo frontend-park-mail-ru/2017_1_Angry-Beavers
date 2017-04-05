316ac4be51039e5835e23962d68f943c446829f0
Ed Asriyan <ed-asriyan@protonmail.com>
2017-04-06 01:10:31 +0300

    Simplify User model

--- public/static/models/user.js
+++ public/static/models/user.js
@@ -1,25 +1,11 @@
+'use strict';
+
 (function () {
     class User {
-
         constructor(opt) {
-            this.responseMap = {
-                200: '1',
-                400: '0',
-                401: '0',
-                403: '0',
-            };
             this.email = opt.email || '';
             this.login = opt.login || '';
             this.score = opt.score || 0;
-            this.isAuth = opt.isAuth || 0;
-            this.responseObj = opt.responseObj || {};
-        }
-
-        setUser(opt = {}) {
-            this.email = opt.email || '';
-            this.login = opt.login || '';
-            this.score = opt.score || 0;
-            this.session = opt.session || '';
         }
     }
 
