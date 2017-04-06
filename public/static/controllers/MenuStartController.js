511a3d558871a8daa78fabd7428cb946a7f6edaf
Ed Asriyan <asriyan1998@yandex.ru>
2017-04-06 03:46:09 +0300

    Turn MenuStartConstoller as ES6 module

--- public/static/controllers/MenuStartController.js
+++ public/static/controllers/MenuStartController.js
@@ -4,54 +4,52 @@
 
 'use strict';
 
-(function () {
-    class MenuStartController extends window.View {
-        constructor(opt = {}) {
-            super(opt);
-            this.addListener();
-        }
-
-        addListener() {
-            this.page_parts.get("AppName").querySelector(".appname").addEventListener('click', event => {
-                event.preventDefault();
-                this.router.go("/");
-            });
-
-            document.getElementById('menuStart_signIn').addEventListener('click', event => {
-                event.preventDefault();
-                this.router.go('/signin');
-            });
-            document.getElementById('menuStart_aboutUs').addEventListener('click', event => {
-                event.preventDefault();
-                this.router.go('/aboutus');
-            });
-            document.getElementById('menuStart_rules').addEventListener('click', event => {
-                event.preventDefault();
-                this.router.go('/rules');
-            });
-        }
-
-        resume() {
-            this.show();
-        }
-
-        show() {
-            this.page_parts.get("AppName").hidden = false;
-            if (this.session.isAuth) {
-                this.router.go('/');
-            }
-            else {
-                this.page_parts.get("MenuStart").hidden = false;
-            }
-            this.page_parts.get("Footer").hidden = false;
-        }
-
-        hide() {
-            this.page_parts.get("AppName").hidden = true;
-            this.page_parts.get("MenuStart").hidden = true;
-            this.page_parts.get("Footer").hidden = true;
-        }
+class MenuStartController extends window.View {
+    constructor(opt = {}) {
+        super(opt);
+        this.addListener();
     }
 
-    window.MenuStartController = MenuStartController;
-}());
+    addListener() {
+        this.page_parts.get("AppName").querySelector(".appname").addEventListener('click', event => {
+            event.preventDefault();
+            this.router.go("/");
+        });
+
+        document.getElementById('menuStart_signIn').addEventListener('click', event => {
+            event.preventDefault();
+            this.router.go('/signin');
+        });
+        document.getElementById('menuStart_aboutUs').addEventListener('click', event => {
+            event.preventDefault();
+            this.router.go('/aboutus');
+        });
+        document.getElementById('menuStart_rules').addEventListener('click', event => {
+            event.preventDefault();
+            this.router.go('/rules');
+        });
+    }
+
+    resume() {
+        this.show();
+    }
+
+    show() {
+        this.page_parts.get("AppName").hidden = false;
+        if (this.session.isAuth) {
+            this.router.go('/');
+        }
+        else {
+            this.page_parts.get("MenuStart").hidden = false;
+        }
+        this.page_parts.get("Footer").hidden = false;
+    }
+
+    hide() {
+        this.page_parts.get("AppName").hidden = true;
+        this.page_parts.get("MenuStart").hidden = true;
+        this.page_parts.get("Footer").hidden = true;
+    }
+}
+
+export default MenuStartController;
