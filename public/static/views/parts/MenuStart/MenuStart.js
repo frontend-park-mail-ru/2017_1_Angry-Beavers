b8d4acb16a3ddf6298457f131cfedc9c61faef2d
Ed Asriyan <ed-asriyan@protonmail.com>
2017-04-06 04:33:06 +0300

    Turn MenuStart as ES6 module

--- public/static/views/parts/MenuStart/MenuStart.js
+++ public/static/views/parts/MenuStart/MenuStart.js
@@ -4,30 +4,30 @@
 
 'use strict';
 
-(function () {
-    const ITEMS = [
-        {
-            id: "menuStart_signIn",
-            defaultImg: "images/Login_btn.png",
-            backImg: "images/GOODFACE.png"
-        },
-        {
-            id: "menuStart_aboutUs",
-            defaultImg: "images/About_us.png",
-            backImg: "images/We_do_it.png"
-        },
-        {
-            id: "menuStart_rules",
-            defaultImg: "images/About_rules.png",
-            backImg: "images/About_rules_2.png"
-        }
-    ];
+const ITEMS = [
+    {
+        id: "menuStart_signIn",
+        defaultImg: "images/Login_btn.png",
+        backImg: "images/GOODFACE.png"
+    },
+    {
+        id: "menuStart_aboutUs",
+        defaultImg: "images/About_us.png",
+        backImg: "images/We_do_it.png"
+    },
+    {
+        id: "menuStart_rules",
+        defaultImg: "images/About_rules.png",
+        backImg: "images/About_rules_2.png"
+    }
+];
 
-    window.MenuStart = function MenuStart() {
-        let div = document.createElement('div');
-        div.innerHTML = pugMenuStart({
-            items: ITEMS
-        });
-        return div;
-    };
-}());
+const MenuStart = function MenuStart() {
+    let div = document.createElement('div');
+    div.innerHTML = pugMenuStart({
+        items: ITEMS
+    });
+    return div;
+};
+
+export default MenuStart;
