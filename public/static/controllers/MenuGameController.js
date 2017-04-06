c607add9f84dda261266d057ec630a441fe04cfe
Ed Asriyan <ed-asriyan@protonmail.com>
2017-04-06 03:52:54 +0300

    Turn MenuGameController as ES6 module

--- public/static/controllers/MenuGameController.js
+++ public/static/controllers/MenuGameController.js
@@ -4,62 +4,58 @@
 
 'use strict';
 
-(function () {
-    class MenuGameController extends window.View {
-        constructor(opt = {}) {
-            super(opt);
-            this.addListener();
-        }
 
-        addListener() {
-            this.page_parts.get("AppName").querySelector(".appname").addEventListener('click', event => {
-                event.preventDefault();
-                this.router.go("/");
-            });
-
-            document.getElementById("menuGame_play").addEventListener('click', event => {
-                event.preventDefault();
-                this.router.go('/play');
-            });
-            document.getElementById("menuGame_score").addEventListener('click', event => {
-                event.preventDefault();
-                this.router.go('/scorelist');
-            });
-            document.getElementById("menuGame_aboutUs").addEventListener('click', event => {
-                event.preventDefault();
-                this.router.go('/aboutus');
-            });
-            document.getElementById("menuGame_rules").addEventListener('click', event => {
-                event.preventDefault();
-                this.router.go('/rules');
-            });
-        }
-
-
-        resume() {
-            this.show();
-        }
-
-        show() {
-            this.page_parts.get("AppName").hidden = false;
-            if (!this.session.isAuth) {
-                this.router.go('/signin');
-            }
-            else {
-                this.page_parts.get("MenuGame").hidden = false;
-            }
-            this.page_parts.get("Footer").hidden = false;
-        }
-
-        hide() {
-            this.page_parts.get("AppName").hidden = true;
-            this.page_parts.get("MenuGame").hidden = true;
-            this.page_parts.get("Footer").hidden = true;
-        }
-
-
+class MenuGameController extends window.View {
+    constructor(opt = {}) {
+        super(opt);
+        this.addListener();
     }
 
-    window.MenuGameController = MenuGameController;
-}());
+    addListener() {
+        this.page_parts.get("AppName").querySelector(".appname").addEventListener('click', event => {
+            event.preventDefault();
+            this.router.go("/");
+        });
 
+        document.getElementById("menuGame_play").addEventListener('click', event => {
+            event.preventDefault();
+            this.router.go('/play');
+        });
+        document.getElementById("menuGame_score").addEventListener('click', event => {
+            event.preventDefault();
+            this.router.go('/scorelist');
+        });
+        document.getElementById("menuGame_aboutUs").addEventListener('click', event => {
+            event.preventDefault();
+            this.router.go('/aboutus');
+        });
+        document.getElementById("menuGame_rules").addEventListener('click', event => {
+            event.preventDefault();
+            this.router.go('/rules');
+        });
+    }
+
+
+    resume() {
+        this.show();
+    }
+
+    show() {
+        this.page_parts.get("AppName").hidden = false;
+        if (!this.session.isAuth) {
+            this.router.go('/signin');
+        }
+        else {
+            this.page_parts.get("MenuGame").hidden = false;
+        }
+        this.page_parts.get("Footer").hidden = false;
+    }
+
+    hide() {
+        this.page_parts.get("AppName").hidden = true;
+        this.page_parts.get("MenuGame").hidden = true;
+        this.page_parts.get("Footer").hidden = true;
+    }
+}
+
+export default MenuGameController;
