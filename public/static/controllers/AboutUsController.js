3297dcff073f7ec1c86a25bc5b109b1e38f9c2d1
Ed Asriyan <ed-asriyan@protonmail.com>
2017-04-06 04:01:16 +0300

    Turn AboutUsController as ES6 module

--- public/static/controllers/AboutUsController.js
+++ public/static/controllers/AboutUsController.js
@@ -1,37 +1,37 @@
 /**
  * Created by pacman29 on 08.03.17.
  */
-(function () {
-    class AboutUsController extends window.View {
-        constructor(opt = {}) {
-            super(opt);
-            this.addListener();
-        }
 
-        addListener() {
-            this.page_parts.get("AppName").addEventListener('click', event => {
-                event.preventDefault();
-                this.router.go("/");
-            });
-        }
+'use strict';
 
-        resume() {
-            this.show();
-        }
-
-        show() {
-            this.page_parts.get("AppName").hidden = false;
-            this.page_parts.get("AboutUs").hidden = false;
-            this.page_parts.get("Footer").hidden = false;
-        }
-
-        hide() {
-            this.page_parts.get("AppName").hidden = true;
-            this.page_parts.get("AboutUs").hidden = true;
-            this.page_parts.get("Footer").hidden = true;
-        }
-
+class AboutUsController extends window.View {
+    constructor(opt = {}) {
+        super(opt);
+        this.addListener();
     }
 
-    window.AboutUsController = AboutUsController;
-}());
+    addListener() {
+        this.page_parts.get("AppName").addEventListener('click', event => {
+            event.preventDefault();
+            this.router.go("/");
+        });
+    }
+
+    resume() {
+        this.show();
+    }
+
+    show() {
+        this.page_parts.get("AppName").hidden = false;
+        this.page_parts.get("AboutUs").hidden = false;
+        this.page_parts.get("Footer").hidden = false;
+    }
+
+    hide() {
+        this.page_parts.get("AppName").hidden = true;
+        this.page_parts.get("AboutUs").hidden = true;
+        this.page_parts.get("Footer").hidden = true;
+    }
+}
+
+export default AboutUsController;
