514d3f6d562dc545f10486ef074028647ba78e54
Ed Asriyan <asriyan1998@yandex.ru>
2017-04-06 03:57:00 +0300

    Turn ScoreListController as ES6 module

--- public/static/controllers/ScoreListController.js
+++ public/static/controllers/ScoreListController.js
@@ -1,46 +1,46 @@
 /**
  * Created by pacman29 on 08.03.17.
  */
-(function () {
-    class ScoreListController extends window.View {
-        constructor(opt = {}) {
-            super(opt);
-            this.addListener();
-        }
 
-        addListener() {
-            this.page_parts.get("AppName").querySelector(".appname").addEventListener('click', event => {
-                event.preventDefault();
-                this.router.go("/");
-            });
-        }
+'use strict';
 
-        resume() {
-            this.show();
-        }
-
-        show() {
-            this.page_parts.get("AppName").hidden = false;
-            if(!this.session.isAuth){
-                this.router.go('/signin');
-            } else {
-                this.page_parts.get("ScoreList").hidden = false;
-            }
-            /*
-             TODO :: Здесь нужно заполнить лидерборд, тут надо просто подумать
-             */
-
-
-            this.page_parts.get("Footer").hidden = false;
-        }
-
-        hide() {
-            this.page_parts.get("AppName").hidden = true;
-            this.page_parts.get("ScoreList").hidden = true;
-            this.page_parts.get("Footer").hidden = true;
-        }
-
+class ScoreListController extends window.View {
+    constructor(opt = {}) {
+        super(opt);
+        this.addListener();
     }
 
-    window.ScoreListController = ScoreListController;
-}());
+    addListener() {
+        this.page_parts.get("AppName").querySelector(".appname").addEventListener('click', event => {
+            event.preventDefault();
+            this.router.go("/");
+        });
+    }
+
+    resume() {
+        this.show();
+    }
+
+    show() {
+        this.page_parts.get("AppName").hidden = false;
+        if (!this.session.isAuth) {
+            this.router.go('/signin');
+        } else {
+            this.page_parts.get("ScoreList").hidden = false;
+        }
+
+        /*
+         TODO :: Здесь нужно заполнить лидерборд, тут надо просто подумать
+         */
+
+        this.page_parts.get("Footer").hidden = false;
+    }
+
+    hide() {
+        this.page_parts.get("AppName").hidden = true;
+        this.page_parts.get("ScoreList").hidden = true;
+        this.page_parts.get("Footer").hidden = true;
+    }
+}
+
+export default ScoreListController;
