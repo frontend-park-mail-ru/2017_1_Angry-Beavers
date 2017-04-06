7592d1f11e75617583f58f047a6a00c649a321ba
Ed Asriyan <asriyan1998@yandex.ru>
2017-04-06 04:36:48 +0300

    Turn ScoreList as ES6 module

--- public/static/views/parts/ScoreList/ScoreList.js
+++ public/static/views/parts/ScoreList/ScoreList.js
@@ -4,42 +4,42 @@
 
 'use strict';
 
-(function () {
-    const RIGHT_IMG = "images/Scorelist.png";
-    const PLAYERS = [
-        {
-            position: "1",
-            nickname: "lol_1",
-            score: "999"
-        },
-        {
-            position: "2",
-            nickname: "lol_2",
-            score: "888"
-        },
-        {
-            position: "3",
-            nickname: "lol_3",
-            score: "777"
-        },
-    ];
-    const YOU = {
-        position: "1337",
-        nickname: "Andrew",
-        score: "-7"
-    };
+const RIGHT_IMG = "images/Scorelist.png";
+const PLAYERS = [
+    {
+        position: "1",
+        nickname: "lol_1",
+        score: "999"
+    },
+    {
+        position: "2",
+        nickname: "lol_2",
+        score: "888"
+    },
+    {
+        position: "3",
+        nickname: "lol_3",
+        score: "777"
+    },
+];
+const YOU = {
+    position: "1337",
+    nickname: "Andrew",
+    score: "-7"
+};
 
-    window.ScoreList = function () {
-        let div = document.createElement('div');
+const ScoreList = function () {
+    let div = document.createElement('div');
 
-        div.innerHTML = pugScoreList({
-            topPlayers: PLAYERS,
-            player: YOU
-        });
+    div.innerHTML = pugScoreList({
+        topPlayers: PLAYERS,
+        player: YOU
+    });
 
-        return window.BorderConstruct({
-            el: div,
-            img: RIGHT_IMG
-        });
-    }
-}());
+    return window.BorderConstruct({
+        el: div,
+        img: RIGHT_IMG
+    });
+};
+
+export default ScoreList;
