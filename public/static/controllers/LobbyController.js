25792d87e477944e91ec4529834a5c5cda49eaa6
Ed Asriyan <ed-asriyan@protonmail.com>
2017-05-06 21:54:12 +0300

    Init Lobby

---
+++ public/static/controllers/LobbyController.js
@@ -0,0 +1,44 @@
+/**
+ * Created by ed on 06.05.17.
+ */
+
+'use strict';
+
+import View from '../modules/view';
+
+class LobbyController extends View {
+    constructor(opt = {}) {
+        if (LobbyController.__instance) {
+            return LobbyController.__instance;
+        }
+        super(opt);
+        LobbyController.__instance = this;
+        this.addListener();
+    }
+
+    addListener() {
+
+    }
+
+
+    resume() {
+        this.show();
+    }
+
+    show() {
+        if (!this.session.isAuth) {
+            this.router.go('/signin');
+        }
+        else {
+            this.page_parts.get("UserHeader").hidden = false;
+            document.getElementById('userheader_login').innerHTML = this.session.user.login;
+            document.getElementById('userheader_score').innerHTML = this.session.user.score;
+        }
+    }
+
+    hide() {
+        this.page_parts.get("UserHeader").hidden = true;
+    }
+}
+
+export default LobbyController;
