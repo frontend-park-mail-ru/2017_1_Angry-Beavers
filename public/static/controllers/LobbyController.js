9722399265c836732370077fa00a26ad354a53d2
Ed Asriyan <asriyan1998@yandex.ru>
2017-05-07 17:36:03 +0300

    Update lobby ui

--- public/static/controllers/LobbyController.js
+++ public/static/controllers/LobbyController.js
@@ -31,16 +31,29 @@ class LobbyController extends View {
         }
         else {
             this.page_parts.get("UserHeader").hidden = false;
+            this.page_parts.get("Lobby").hidden = false;
             document.getElementById('userheader_login').innerHTML = this.session.user.login;
             document.getElementById('userheader_score').innerHTML = this.session.user.score;
-
+            document.getElementById('lobby_users').innerHTML = '';
             let lobby = this.session.createLobby();
             lobby.onInfo = function (data) {
-                alert(JSON.stringify(data));
+                document.getElementById('lobby_title').innerHTML = `Ожидаем игроков (${data.maxNumber})`;
+                data.users.forEach(u => {
+                    let user = document.createElement('label');
+                    user.innerHTML = u.userLogin;
+                    document.getElementById('lobby_users').appendChild(user);
+                    document.getElementById('lobby_users').appendChild(document.createElement('hr'));
+                });
             };
             lobby.onClosed = (function () {
                 this.router.go('/');
             }).bind(this);
+            lobby.onUserAdd = function (data) {
+                let user = document.createElement('label');
+                user.innerHTML = data.user.userLogin;
+                document.getElementById('lobby_users').appendChild(user);
+                document.getElementById('lobby_users').appendChild(document.createElement('hr'));
+            };
 
             lobby.start();
         }
@@ -48,6 +61,7 @@ class LobbyController extends View {
 
     hide() {
         this.page_parts.get("UserHeader").hidden = true;
+        this.page_parts.get("Lobby").hidden = true;
     }
 }
 
