df037f73addcc3af5c9b7bc62a593496ca2d1b90
Ed Asriyan <asriyan1998@yandex.ru>
2017-05-07 16:47:56 +0300

    Connect controller with lobby

--- public/static/models/lobby.js
+++ public/static/models/lobby.js
@@ -4,6 +4,8 @@
 
 'use strict';
 
+import Game from './game';
+
 class Lobby {
     constructor(session) {
         this._session = session;
@@ -11,18 +13,67 @@ class Lobby {
 
     start() {
         let ws = new WebSocket('wss://jokinghazardserver.herokuapp.com/lobby');
-        ws.onopen = function () {
-            alert("Connection opened...");
-        };
-        ws.onclose = function () {
-            alert("Connection closed...");
-        };
-        ws.onmessage = function (evt) {
-            alert(evt.data);
-        };
-        ws.onerror = function () {
-            alert("Error anaui opened...");
-        };
+        ws.onclose = (function () {
+            this._onClosed && this._onClosed();
+        }).bind(this);
+        ws.onmessage = (function (evt) {
+            let data = JSON.parse(evt.data);
+            switch (data.type) {
+                case 'Lobby Info':
+                    this._onInfo && this._onInfo(data);
+                    break;
+                case 'UserAddedMessage':
+                    this._onUserAdd && this._onUserAdd(data);
+                    break;
+                case 'GameReadyMessage':
+                    this._onGameStart && this._onGameStart(new Game(this._session));
+                    break;
+
+            }
+        }).bind(this);
+        ws.onerror = (function () {
+            this._onError && this._onError();
+        }).bind(this);
+    }
+
+    get onInfo() {
+        return this._onInfo;
+    }
+
+    set onInfo(value) {
+        this._onInfo = value;
+    }
+
+    get onUserAdd() {
+        return this._onUserAdd;
+    }
+
+    set onUserAdd(value) {
+        this._onUserAdd = value;
+    }
+
+    get onGameStart() {
+        return this._onGameStart;
+    }
+
+    set onGameStart(value) {
+        this._onGameStart = value;
+    }
+
+    get onError() {
+        return this._onError;
+    }
+
+    set onError(value) {
+        this._onError = value;
+    }
+
+    get onClosed() {
+        return this._onClosed;
+    }
+
+    set onClosed(value) {
+        this._onClosed = value;
     }
 }
 
