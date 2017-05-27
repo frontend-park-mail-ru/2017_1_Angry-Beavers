25f75ef04bf6ee8218c474311d1ec3d45397197a
Ed Asriyan <asriyan1998@yandex.ru>
2017-05-27 15:02:00 +0300

    Add https redirect

--- app/main.js
+++ app/main.js
@@ -1,3 +1,7 @@
+if (window.location.hostname !== 'localhost' && window.location.protocol !== 'https:') {
+    window.location = 'https://' + window.location.hostname + window.location.pathname + window.location.hash;
+}
+
 import 'bootstrap-loader';
 
 import SwLoader from './modules/Swloader';
