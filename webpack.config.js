087c829860caf8388e78d2f57e3e2ca2e8f6078d
Pacman29 <balyakin.d.v@gmail.com>
2017-05-28 23:09:20 +0300

    test imagescontroller

--- webpack.config.js
+++ webpack.config.js
@@ -59,7 +59,7 @@ module.exports = {
             $: "jquery",
             jQuery: "jquery",
             "window.jQuery": "jquery",
-        })//,
-        //new webpack.optimize.UglifyJsPlugin()
+        }),
+        new webpack.optimize.UglifyJsPlugin()
     ]
 };
