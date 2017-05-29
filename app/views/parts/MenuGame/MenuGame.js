95746bdd7098096544914e4661d1ecfc4341fd51
pacman29 <balyakin.d.v@gmail.com>
2017-05-29 17:00:55 +0300

    merge

--- app/views/parts/MenuGame/MenuGame.js
+++ app/views/parts/MenuGame/MenuGame.js
@@ -11,25 +11,6 @@ import imagescontroller from '../../ImagesController';
 const ITEMS = [
     {
         id: "menuGame_play",
-<<<<<<< HEAD
-        defaultImg: "/images/Game.webp",
-        backImg: "/images/Game_hold.webp"
-    },
-    {
-        id: "menuGame_score",
-        defaultImg: "/images/Score_btn.webp",
-        backImg: "/images/Score_btn_hold.webp"
-    },
-    {
-        id: "menuGame_aboutUs",
-        defaultImg: "/images/About_us.webp",
-        backImg: "/images/We_do_it.webp"
-    },
-    {
-        id: "menuGame_rules",
-        defaultImg: "/images/About_rules.webp",
-        backImg: "/images/About_rules_2.webp"
-=======
         defaultImg: imagescontroller.get_menu_img("Game"),
         backImg: imagescontroller.get_menu_img("Game_hold")
     },
@@ -47,7 +28,6 @@ const ITEMS = [
         id: "menuGame_rules",
         defaultImg: imagescontroller.get_menu_img("About_rules"),
         backImg: imagescontroller.get_menu_img("About_rules_2")
->>>>>>> baacede1307f7c2bbf11eba3d49f0ccb6063e6d7
     }
 ];
 
